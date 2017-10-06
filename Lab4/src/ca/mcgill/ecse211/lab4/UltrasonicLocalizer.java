a65d95e338da47032365013ff597174e2a32b76f
LiTigre <li.zhang17@mail.mcgill.ca>
2017-10-06 16:47:30 -0400

    trying my script

--- Lab4/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
+++ Lab4/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
@@ -9,6 +9,12 @@ package ca.mcgill.ecse211.lab4;
  * the local maximum is halfway between the intersections of the curve and the line y=d
  * (the constant d is chosen, following experimentation) 
  * 
+ * knowing the heading of the robot when it drops below d and when it rises above d
+ * would let you know how far you are from the corner
+ * 
+ * but because of noise, we need a noise margin
+ * so if the distance falls below d+k, then you enter the noise margin
+ * however, only when the distance falls below d-k do you actually have a falling edge
  * 
  * 
  * @author LiTigre
