16a5c9cc4815cd73e0f5fce74faae324609c5780
LiTigre <li.zhang17@mail.mcgill.ca>
2017-10-06 17:21:01 -0400

    Fri  6 Oct 2017 17:21:00 EDT

--- Lab4/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
+++ Lab4/src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
@@ -16,12 +16,40 @@ package ca.mcgill.ecse211.lab4;
  * so if the distance falls below d+k, then you enter the noise margin
  * however, only when the distance falls below d-k do you actually have a falling edge
  * 
+ * then, you can choose the angle to between the angle that you enter the noise margin
+ * and when you exit it(aka have an actual falling edge)
  * 
+ * two scenarios:
+ * 1. starts !facing the wall
+ * 		- detect falling edge, switch direction, then detect another falling edge
+ * 		- detect falling edge, continue, detect rising edge
+ * 2. starts facing the wall
+ * 		- rising edge, switch, rising edge
+ * 		- rising edge, continue, falling edge
+ * 
+ * #####be careful of wraparound for angles#####
+ * 
+ * a, and b being the angles at which you detect the wall
+ * if ( a < b ) {
+ * 		dTheta = 45 - (( a+b )/2)
+ * }
+ * else {
+ * 		dTheta = 225 - (( a+b )/2)
+ * }
+ * dTheta = angle to add to the heading given by odometer
+ * (setTheta = getTheta + dTheta)
  * 
  * @author LiTigre
  *
  */
 
 public class UltrasonicLocalizer {
-
+	//TODO: create fallingEdge()
+	//TODO: create risingEdge()
+	
+	
+	
+	
+	
+	
 }
